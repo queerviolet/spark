ccad440a4d3fc1f147e79c1d1cb17981c46bb167
Ashi Krishnan <me@ashi.works>
2017-11-09 15:42:49 -0500

    Webpack config works with firebase serve.

---
+++ main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {render} from 'react-dom'
+import {AppContainer} from 'react-hot-loader';
+
+import App from './App'
+
+function main() {
+  render(
+    <AppContainer>
+      <App />
+    </AppContainer>,
+    document.getElementById('main'))
+}
+
+main()
+
+module.hot && module.hot.accept('./App', main)
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,75 @@
 const webpack = require('webpack')
 
-module.exports = {
-  entry: './app.js',
+const config = env => ({
+  entry: entries(env, './main.js'),
   output: {
     filename: 'bundle.js',
-    publicPath: '/',
+    path: `${__dirname}/public`,
   },
   devtool: 'source-map',
+  resolve: {
+    extensions: [ '.jsx', '.js', '.json' ],
+  },
+  devServer: devServer(env),
   module: {
     rules: [{
       test: /jsx?$/,
-      exclude: /(node_modules|bower_components)/,
-      use: ['babel-loader']
+      exclude: /node_modules/,
+      use: babel(env),
+    },
+    {
+      test: /\.(jpeg|jpg|png|)$/,
+      use: 'url-loader',
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    },
+    {
+      test: /\.(txt|md|markdown)$/,
+      use: 'raw-loader',
     }]
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin,  // enable HMR globally
-    new webpack.NamedModulesPlugin,          // Better module names in the browser
-                                              // console on HMR updates
-    new webpack.NoEmitOnErrorsPlugin,        // Don't emit on errors.
-  ],
+  plugins: plugins(env),
+})
+
+const isProd = ({NODE_ENV}) => NODE_ENV === 'production'
+const isHot = env => !isProd(env)
+
+const entries = (env, entry) =>
+  isHot(env)
+    ? ['react-hot-loader/patch', entry]
+    : entry
+
+const plugins = env => isHot(env) ? [
+  new webpack.HotModuleReplacementPlugin,  // Enable HMR globally
+  new webpack.NamedModulesPlugin,          // Better module names in the browser
+                                           // console on HMR updates
+  new webpack.NoEmitOnErrorsPlugin,        // Don't emit on errors.
+] : []
+
+function devServer(env) {
+  if (isProd(env)) return
+  const {FIREBASE_SERVE_URL} = env  
+  console.log('FIREBASE_SERVE_URL', FIREBASE_SERVE_URL)
+  return {
+    hot: true,
+    proxy: FIREBASE_SERVE_URL && {
+      "/": FIREBASE_SERVE_URL
+    }
+  }
 }
+
+const babel = env => ({
+  loader: 'babel-loader', 
+  options: {
+    presets: [
+      ['env', {modules: false}],
+      'stage-2',
+      'react',
+    ],
+    plugins: isHot(env) && ['react-hot-loader/babel']
+  }
+})
+
+module.exports = config(process.env)
