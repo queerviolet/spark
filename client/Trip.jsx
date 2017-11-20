709be34e7b8cb6116eaf972f6f633e051090057e
April Rueb <aprilrueb@gmail.com>
2017-11-20 11:51:32 -0500

    associating ze things

--- client/Trip.jsx
+++ client/Trip.jsx
@@ -3,9 +3,15 @@ import { db } from '../fire'
 import Chat from './Chat'
 import Pinned from './Pinned'
 
-export default (props) => (
-    <div>
-        <Chat room={db.collection('test-chat')} user={props.user}/>
-        <Pinned room={db.collection('test-event')} />
-    </div>
-)
+export default class Trip extends Component {
+
+        render(){
+            return (
+            <div>
+                <Chat room={db.collection('trips').doc(this.props.match.params.tripId).collection('chat')} user={this.props.user}/>
+                <Pinned room={db.collection('test-event')} />
+                <p>{this.props.match.params.tripId}</p>
+            </div>
+            )
+        }
+    }
