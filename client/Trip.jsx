f87d0055158aab014e34245c2e529a86c7a930d7
April Rueb <aprilrueb@gmail.com>
2017-11-20 12:34:53 -0500

    checks if user is part of trip

--- client/Trip.jsx
+++ client/Trip.jsx
@@ -5,12 +5,42 @@ import Pinned from './Pinned'
 
 export default class Trip extends Component {
 
-        render(){
-            return (
-            <div>
-                <Chat room={db.collection('trips').doc(this.props.match.params.tripId).collection('chat')} user={this.props.user}/>
-                <Pinned room={db.collection('trips').doc(this.props.match.params.tripId).collection('event')} user={this.props.user}/>
-            </div>
-            )
+    constructor(){
+        super()
+        this.state = {
+            isPartOfTrip: false
         }
     }
+
+    componentDidMount(){
+        const tripRef = db.collection('trips').doc(this.props.match.params.tripId);
+
+        tripRef.get().then(doc => {
+            if (doc.exists && doc.data().users[this.props.user.uid]) {
+                this.setState( {isPartOfTrip: true} );
+                console.log("Document data:", doc.data().users[this.props.user.uid]);
+            } else {
+                console.log("No such document!");
+            }
+        }).catch(error => {
+            console.log("Error getting document: ", error);
+        })
+    }
+
+    render(){
+        const tripRef = db.collection('trips').doc(this.props.match.params.tripId);
+        let isPartOfTrip = this.state.isPartOfTrip;
+
+        return (
+            isPartOfTrip ?
+            <div>
+                <Chat room={tripRef.collection('chat')} user={this.props.user}/>
+                <Pinned room={tripRef.collection('event')} user={this.props.user}/>
+            </div>
+            :
+            <div>
+                You need to be invited to this trip!
+            </div>
+        )
+    }
+}
