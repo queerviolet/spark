6c9a72ab1e8fedbf680b71bfdf1d932d8023b592
Annelise Thorsen <annelise.thorsen@gmail.com>
2017-11-20 18:32:10 -0500

    places render 20 options

--- client/GetGeo.jsx
+++ client/GetGeo.jsx
@@ -6,20 +6,13 @@ export function getCoords(city) {
         .then((res) => res.json())
         .then((res) => (res.results[0].geometry.location))
         .then((coords) => getActivityTypes(coords))
-        //(coords.lat).toString()+","+(coords.lng).toString())
-        // .then((parsedCoords) => getActivityTypes(parsedCoords))
-       // .then((parsedCoords) => getActivityTypes())
 }
 
 export function getActivityTypes(coords) {
-        // console.log("WE ARE IN GETACTIVITYTYPES:" , coords)
         var lat = coords.lat
-        // console.log("lat", lat)
         var lng = coords.lng
-        // console.log('lng', lng)
 
-        // fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=500&rankby=prominence&key=AIzaSyAcMEPVIQJrhI3lacqATvGh50bS6gr-lJI`)
-        fetch('https://cors-anywhere.herokuapp.com/'+`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.712775,-74.005973&radius=500&key=AIzaSyDfFiJgPk0kIAla_Csuz7wXh7M0cWtb1Yg`)
+        fetch('https://cors-anywhere.herokuapp.com/'+`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=500&rankby=prominence&key=AIzaSyDfFiJgPk0kIAla_Csuz7wXh7M0cWtb1Yg`)        
         .then((res) => res.json())
         .then((res) => (res.results))
         .then((res) => console.log(res))
