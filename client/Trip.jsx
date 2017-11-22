25e65df3573fe053bc0f1397276f60c2d2fd7423
Caryn McCarthy <cmccarthy15@gmail.com>
2017-11-22 12:48:15 -0500

    sends an email using a cloud function'
    :

--- client/Trip.jsx
+++ client/Trip.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { db } from '../fire'
 import {Chat, Pinned, Itinerary} from './index'
 
+
 export default class Trip extends Component {
 
     constructor(){
@@ -9,8 +10,11 @@ export default class Trip extends Component {
         this.state = {
             isPartOfTrip: false,
             startDate: {},
-            endDate: {}
+            endDate: {},
+            name: '',
+            showInvite: false
         }
+        this.sendInvite = this.sendInvite.bind(this);
     }
 
     componentDidMount(){
@@ -18,8 +22,8 @@ export default class Trip extends Component {
 
         tripRef.get().then(doc => {
             if (doc.exists && doc.data().users[this.props.user.uid]) {
-                const { startDate, endDate } = doc.data();
-                this.setState( {isPartOfTrip: true, startDate, endDate } );
+                const { startDate, endDate, name } = doc.data();
+                this.setState( {isPartOfTrip: true, startDate, endDate, name } );
                 // console.log('this state is: ', this.state)
                 // console.log("Document data:", doc.data().users[this.props.user.uid]);
             } else {
@@ -30,21 +34,48 @@ export default class Trip extends Component {
         })
     }
 
+    /* NOTES: it would be cool if it rendered "sent mail" for a second */
+    sendInvite(evt){
+        evt.preventDefault();
+        //target email is evt.target.toEmail.value
+
+        console.log('invite target', evt.target.toEmail.value);
+        db.collection('users')
+            .doc(this.props.user.uid)
+            .set({ invitee: evt.target.toEmail.value}, {merge: true});
+        evt.target.toEmail.value = '';
+        this.setState({showInvite: false});
+
+    }
+
     render(){
         const tripRef = db.collection('trips').doc(this.props.match.params.tripId);
         let isPartOfTrip = this.state.isPartOfTrip;
 
-
         return (
             isPartOfTrip ?
-            <div className="flex-row-wrap around">
-                <Chat room={tripRef.collection('chat')} user={this.props.user} />
-                <Itinerary
-                    room={tripRef.collection('event')}
-                    user={this.props.user}
-                    startDate={this.state.startDate}
-                    endDate = {this.state.endDate} />
-                <Pinned room={tripRef.collection('event')} user={this.props.user} />
+            <div>
+                <div className="trip-header">
+                    <h1>{this.state.name}</h1>
+                    <button onClick={() => this.setState({showInvite: !this.state.showInvite})}>Invite!</button>
+                    {this.state.showInvite &&
+                        <form onSubmit={this.sendInvite}>
+                            <label>
+                                <input type="text" name="toEmail" />
+                            </label>
+                            <input type="submit" value="Submit" />
+                        </form>
+                    }
+                </div>
+                <div className="flex-row-wrap around">
+                    <Chat room={tripRef.collection('chat')} user={this.props.user} />
+                    <Itinerary
+                        room={tripRef.collection('event')}
+                        user={this.props.user}
+                        startDate={this.state.startDate}
+                        endDate = {this.state.endDate} />
+                    <Pinned room={tripRef.collection('event')} user={this.props.user} />
+                </div>
             </div>
             :
             <div>
--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,22 @@
 const functions = require('firebase-functions')
     lib = require('./lib')
 
+const sendmail = require('sendmail')();
+
+
+const sendInvite = (email) => {
+    return sendmail({
+        from: 'joinatrip@triphub.notasite',
+        to: email,
+        subject: 'test sendmail',
+        html: 'Mail of test sendmail ',
+    }, function (err, reply) {
+        console.log(err && err.stack);
+        console.dir(reply);
+    });
+};
+
+
 exports.helloWorld = functions.https.onRequest((request, response) =>
    response.send(lib))
 
@@ -10,7 +26,8 @@ exports.bot = functions.firestore
         console.log('yo.')
         const msg = event.data.data()
         const chat = event.data.ref.parent
-        console.log(msg)
+        console.log('event is: ', event);
+        console.log('message is: ', msg)
         if (msg.text.startsWith('/')) {
             const cmd = msg.text.slice(1)
             console.log(cmd)
@@ -21,4 +38,18 @@ exports.bot = functions.firestore
                 text: cmd
             })
         }
+    })
+
+exports.sendInvite = functions.firestore
+    .document('/users/{userId}')
+    .onUpdate(event => {
+        const user = event.data.data();
+        const invitee = user.invitee;
+
+        if (user.invitee === '') {
+            return;
+        }
+        sendInvite(invitee);
+        return event.data.set({invitee: ''}, {merge: true});
+
     })
