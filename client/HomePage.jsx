fe7ba205137f24b60f4968506916cce86f68e944
April Rueb <aprilrueb@gmail.com>
2017-11-26 21:50:49 -0500

    homepage working

--- client/HomePage.jsx
+++ client/HomePage.jsx
@@ -3,36 +3,23 @@ import { Carousel } from 'react-materialize';
 
 export const HomePage = ({login}) => {
   return (
+
     <div>
-      <h1 className="hp-header"><strong>tripHub</strong></h1>
-      <button className="btn" onClick={login}>Log In</button>
-      <Carousel
-        options={{ fullWidth: true }}
-        images={[
-          'paris.jpg',
-          'iceland.jpg',
-          'hiking.jpg'
-        ]}
-      />
+    <h1 className="hp-header"><strong>tripHub</strong></h1>
+    <button className="btn black" onClick={login}>Log In</button>
+    <Carousel
+      className="carousel"
+      options={{ fullWidth: true }}
+      images={[
+        'paris.jpg',
+        'iceland.jpg',
+        'hiking.jpg',
+        'nyc.jpg'
+      ]}
+    />
     </div>
+
   );
 };
 
 export default HomePage;
-
-/*
-import { Carousel } from 'react-responsive-carousel';
-import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
-
-      <Carousel infiniteLoop={true} autoPlay={true} interval={6000} transitionTime={1000} showThumbs={false} dynamicHeight={true} showStatus={false} height={'600px'}>
-        <div>
-            <img src="http://localhost:8080/ny.jpg" />
-        </div>
-        <div>
-            <img src="http://localhost:8080/beach.jpg" />
-        </div>
-        <div>
-            <img src="http://localhost:8080/paris.jpg" />
-        </div>
-      </Carousel>
-*/
