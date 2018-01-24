3c3e67baf6effcce24c500253a978bd5e5fe25b9
Ashi Krishnan <me@ashi.works>
2018-01-24 15:38:04 -0500

    Functions should be in strict mode. Fix a comma thing.

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
+'use strict'
+
 const functions = require('firebase-functions')
-    lib = require('./lib')
+    , lib = require('./lib')
 
 exports.helloWorld = functions.https.onRequest((request, response) =>
    response.send(lib))
